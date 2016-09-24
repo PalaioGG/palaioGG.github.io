9eaca93f20540737968de9517b8fd424f217d270
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-09-24 18:30:37 +0300

    Copy portfolio for testing purposes

---
+++ js/mainController.js
@@ -0,0 +1,17 @@
+﻿var myApp = angular.module('palaioApp', ['gridshore.c3js.chart']);
+
+myApp.controller('palaioController', [
+    '$scope',
+    function ($scope) {
+        $scope.spice = 'very';
+
+        $scope.chiliSpicy = function () {
+            $scope.spice = 'chili';
+        };
+
+        $scope.jalapenoSpicy = function () {
+            $scope.spice = 'jalapeño';
+        };
+    }
+]);
+
