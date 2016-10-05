557dcd640489cadb1c5a6c264fffe9d85c01551d
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-10-05 21:00:56 +0300

    New portfolio

--- js/mainController.js
+++ js/mainController.js
@@ -3,15 +3,35 @@
 myApp.controller('palaioController', [
     '$scope',
     function ($scope) {
-        $scope.spice = 'very';
+        
+        //$(document).ready(function () {
+        //    $('button').click(function () {
+        //        var stripes = $('#stripe_top');
+        //        stripes.css("border-top", '5px dashed black');
+        //        stripes.animate({transform: 'rotate(90deg)'}, 'slow');
+        //    });
+        //});
 
-        $scope.chiliSpicy = function () {
-            $scope.spice = 'chili';
+        $scope.menu_visible = false;
+
+        $scope.controlNav = function () {
+            var nav = document.getElementById("myNav");
+            var ver = document.getElementById("version");
+
+            if ($scope.menu_visible === false) {
+                $scope.menu_visible = true;
+
+                nav.style.width = "100%";
+                ver.style.color = "white";
+            }
+            else {
+                $scope.menu_visible = false;
+                nav.style.width = "0%";
+                ver.style.color = "black";
+            };
         };
 
-        $scope.jalapenoSpicy = function () {
-            $scope.spice = 'jalapeño';
-        };
+
     }
 ]);
 
