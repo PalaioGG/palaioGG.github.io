88476e82c4f5ae9069bf2acaa219b14e1b53ecf0
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-10-29 19:38:57 +0300

    Update skills section and menu

--- js/mainController.js
+++ js/mainController.js
@@ -11,6 +11,15 @@ myApp.controller('palaioController', [
             var nav = document.getElementById("myNav");
             var ver = document.getElementById("version");
             var con_msg = document.getElementById("construction_msg");
+            var body = document.getElementById("body");
+
+            $scope.closeNav = function () {
+                nav.style.width = "0%";
+                ver.style.color = "black";
+                con_msg.style.display = "none";
+                $scope.menu_visible = false;
+                body.style.overflow = "initial";
+            };
 
             if ($scope.menu_visible === false) {
                 $scope.menu_visible = true;
@@ -18,39 +27,33 @@ myApp.controller('palaioController', [
                 nav.style.width = "100%";
                 ver.style.color = "white";
                 con_msg.style.display = "block";
+                body.style.overflow = "hidden";
             }
             else {
-                $scope.menu_visible = false;
-                nav.style.width = "0%";
-                ver.style.color = "black";
-                con_msg.style.display = "none";
+                $scope.closeNav();
             };
         };
 
-        angular.element($window).bind('resize', function () {
-            var x = 200;
-            var y =  Math.round(window.innerWidth / 7);
-            if (y < 200 && y > 60) {
-                $scope.$apply(function () {
-                    $scope.radiusResize = y;
-                });
-            };
-            console.log('y: ' + y);
-            console.log('radius: ' + $scope.radius);
-        });
+        //angular.element($window).bind('resize', function () {
+        //    var x = 200;
+        //    var y = Math.round(window.innerWidth / 7);
+        //    if (y < 200 && y > 60) {
+        //        $scope.$apply(function () {
+        //            $scope.radiusResize = y;
+        //        });
+        //    };
+        //    //console.log('y: ' + y);
+        //    //console.log('radius: ' + $scope.radius);
+        //});
 
-        //console.log($(window).scrollTop());
+        //$scope.$watch('radiusResize', function (newValue, oldValue) {
+        //    $scope.radius = $scope.radiusResize;
+        //    //console.log('WATCHradius: ' + $scope.radius);
+        //});
 
-        //console.log(window.innerWidth);
-
-        $scope.$watch('radiusResize', function(newValue, oldValue) {
-            $scope.radius = $scope.radiusResize;
-            console.log('WATCHradius: ' + $scope.radius);
-        });
-
-        if (window.innerWidth < 540) {
-            $scope.radius = 150;
-        } else $scope.radius = 200;
+        //if (window.innerWidth < 540) {
+        //    $scope.radius = 150;
+        //} else $scope.radius = 200;
 
         $scope.HTML = { name: 'HTML', percent: 74 };
         $scope.CSS = { name: 'CSS', percent: 72 };
@@ -58,58 +61,81 @@ myApp.controller('palaioController', [
         $scope.responsive = { name: 'Responsive', percent: 66 };
         $scope.javascript = { name: 'Javascript', percent: 54 };
         $scope.angular = { name: 'AngularJS', percent: 48 };
-
+        $scope.data = [
+            { name: 'HTML', percent: 74 },
+        ];
     }
 ])
 
-myApp.directive('d3Donut', function () {
+myApp.directive('d3Donut', function ($window) {
     return {
         restrict: 'EΑ',
         scope: {
-            radius: '=',
             percent: '='
         },
         link: function (scope, element, attrs) {
 
-            var radius = scope.radius
             var percent = scope.percent;
             var text = scope.text;
-            var container = $('.skill-container');
+            var container = $('.donut-container');
             var width = container.width();
             var height = container.height();
+            var container_radius = (width + height) / 2;
+            var radius;
+            var screen_width = $window.innerWidth;
+                
+            if (screen_width > 1920) {
+                radius = container_radius * 0.65;
+            }
+            else if ((screen_width < 992) && (screen_width > 480) ) {
+                radius = container_radius * 1.6;
+            }
+            else if (screen_width < 480) {
+                radius = container_radius * 2;
+            }
+            else radius = container_radius;
+
 
             var svg = d3.select(element[0])
-			.append('svg')
-			.attr('width', radius / 2 + 'px')
-			.attr('height', radius / 2 + 'px');
+            .append('svg')
+            //.attr('width', width / 2 + 'px')
+            //.attr('height', height / 2 + 'px');
+            .attr("preserveAspectRatio", "xMinYMin meet")
+            .attr("viewBox", "-11 -2 80 80")
+            .classed("svg-content", true);
 
             var donutScale = d3.scale.linear().domain([0, 100]).range([0, 2 * Math.PI]);
             var color = "#FFF3C1";
             var data = [[0, 100, "transparent"], [0, percent, color]];
 
             var arc = d3.svg.arc()
-			.innerRadius(radius / 6)
-			.outerRadius(radius / 4)
-			.startAngle(function (d) { return donutScale(d[0]); })
-			.endAngle(function (d) { return donutScale(d[1]); });
+            //.innerRadius(radius / 6)
+            //.outerRadius(radius / 4)
+            .innerRadius(radius / 6)
+            .outerRadius(radius / 4)
+            .startAngle(function (d) { return donutScale(d[0]); })
+            .endAngle(function (d) { return donutScale(d[1]); });
 
             svg.selectAll("path")
-			.data(data)
-			.enter()
-			.append("path")
-			.attr("d", arc)
-			.style("fill", function (d) { return d[2]; })
-			.style("stroke", "#403D30")
-			.attr("transform", "translate(" + radius / 4 + "," + radius / 4 + ")");
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("d", arc)
+            .style("fill", function (d) { return d[2]; })
+            .style("stroke", "#403D30")
+            //.attr("transform", "translate(" + radius / 4 + "," + radius / 4 + ")");
+            .attr("transform", "translate(" + radius / 4 + "," + radius / 4 + ")");
 
             svg.append("text")
-			.attr("x", radius / 4)
-			.attr("y", radius / 4)
-			.attr("dy", ".35em")
-			.attr("text-anchor", "middle")
-			.attr("font-size", "18px")
-			.style("fill", color)
-			.attr("text-anchor", "middle")
+            //.attr("x", radius / 4)
+            //.attr("y", radius / 4)
+            .attr("x", radius / 4)
+            .attr("y", radius / 4)
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .attr("font-size", radius / 7 + "px")
+            .style("fill", color)
+            .attr("text-anchor", "middle")
             .text(percent + '%');
 
         }
